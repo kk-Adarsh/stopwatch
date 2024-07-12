05d7bda7951f494300447bab3f2cdc41646422c0
kk-Adarsh <adarshpandey301199@gmail.com>
2024-07-12 12:48:41 +0530

    Stopwatch Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { FaPlay, FaPause, FaStop, FaClockRotateLeft } from "react-icons/fa6";
+import { GrPowerReset } from "react-icons/gr";
 
 function App() {
+  const [time, setTime] = useState(0);
+
+  const [isRunning, setIsRunning] = useState(false);
+  const [isStopped, setIsStopped] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+    if (isRunning) {
+      intervalId = setInterval(() => setTime(time + 1), 10);
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning, time]);
+
+  const hours = Math.floor(time / 360000);
+
+  const minutes = Math.floor((time % 360000) / 6000);
+
+  const seconds = Math.floor((time % 6000) / 100);
+
+  const milliseconds = time % 100;
+
+  const startAndPause = () => {
+    if (isStopped) {
+      setTime(0);
+      setIsStopped(false);
+    }
+    setIsRunning(!isRunning);
+  };
+  const Stop = () => {
+    setIsRunning(false);
+    setIsStopped(true);
+  };
+
+  const reset = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="stopwatch-container">
+      <p className="stopwatch-time">
+        {hours}:{minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}:
+        {milliseconds.toString().padStart(2, "0")}
+      </p>
+      <div className="stopwatch-buttons">
+        <button
+          className={`button  items-center ${
+            isRunning ? "orange-button" : "green-button"
+          }`}
+          type="button"
+          onClick={startAndPause}
         >
-          Learn React
-        </a>
-      </header>
+          {isRunning ? (
+            <>
+              Pause <FaPause />
+            </>
+          ) : (
+            <>
+              Start <FaPlay />
+            </>
+          )}
+        </button>
+        <button
+          className="button violet-button items-center"
+          type="button"
+          onClick={Stop}
+        >
+          Stop <FaStop />
+        </button>
+        <button
+          className="button red-button items-center"
+          type="button"
+          onClick={reset}
+        >
+          Reset <FaClockRotateLeft className="stroke-custom" />
+        </button>
+      </div>
     </div>
   );
 }
